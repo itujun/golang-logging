dc8e7747c358e84dbc68be8f52ec90e3c2e35bb1
Juna <itujun7@gmail.com>
2025-07-30 03:10:58 +0700

    formatter

---
+++ formatter_test.go
@@ -0,0 +1,18 @@
+package golang_logging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestFormatter(t *testing.T) {
+	logger := logrus.New()
+
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	logger.Info("Hello Info")
+	logger.Warn("Hello Warn")
+	logger.Error("Hello Error")
+
+}
