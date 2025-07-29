a7319f28ed5c323f77d94d4fbf15218c2e6bc1bd
Juna <itujun7@gmail.com>
2025-07-30 03:33:27 +0700

    hook

---
+++ hook_test.go
@@ -0,0 +1,31 @@
+package golang_logging
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+type SampleHook struct {
+}
+
+func (s *SampleHook) Levels() []logrus.Level {
+	return []logrus.Level{logrus.ErrorLevel, logrus.WarnLevel}
+}
+
+func (s *SampleHook) Fire(entry *logrus.Entry) error {
+	// do something
+	fmt.Println("Sample Hook", entry.Level, entry.Message);
+	return nil
+}
+
+func TestHook(t *testing.T) {
+	logger := logrus.New()
+
+	logger.AddHook(&SampleHook{})
+
+	logger.Info("Hello Hook")
+	logger.Warn("Hello Warn")
+	logger.Error("Hello Error")
+}
